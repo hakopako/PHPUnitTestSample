1fc8afb8e79d7d356e006c2836245c2ebffc7311
hakopako <hakohakohako.0518@gmail.com>
2014-05-04 22:13:59 +0900

    initial commit.

---
+++ program.php
@@ -0,0 +1,21 @@
+<?php
+class Program {
+
+	public function plusone($base)
+	{
+		return $base + 1;
+	}
+
+	public function times($base, $times)
+	{
+		return $base * $times;
+	}
+
+	public function negative($base)
+	{
+		return $base * (-1);
+	}
+
+}
+
+?>
---
+++ test/programTest.php
@@ -0,0 +1,47 @@
+<?php
+require_once(dirname(__FILE__) . '/../program.php');
+
+class ProgramTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp() {
+	}
+
+	public function tearDown() {
+	}
+
+	public function testPlusone()
+	{
+		$expected = 3;
+		$base = 2;
+
+		$target = new Program;
+		$result = $target->plusone($base);
+
+		$this->assertEquals($expected, $result);
+	}
+
+	public function testTimes()
+	{
+		$expected = 15;
+		$base = 3;
+		$times = 5;
+
+		$target = new Program;
+		$result = $target->times($base, $times);
+
+		$this->assertEquals($expected, $result);
+	}
+
+	public function testNegative()
+	{
+		$expected = -3;
+		$base = 3;
+
+		$target = new Program;
+		$result = $target->negative($base);
+
+		$this->assertEquals($expected, $result);
+	}
+
+}
+?>
